35610fa4653460a9fed9b78317e4b59988abad80
Artusamak <julien@happyculture.coop>
2016-08-26 12:30:17 +0200

    Add the help function.

--- build.sh
+++ build.sh
@@ -14,6 +14,33 @@ BACKUP_BASE=1;
 URI="https://example.org/"
 
 ########## FUNCTION ##############
+# Help function.
+usage() {
+  bold=$(tput bold)
+  normal=$(tput sgr0)
+
+  echo 'Usage:'
+  echo 'Long version: ./build.sh --env dev --mode install --backup 1 --uri http://hc.fun'
+  echo 'Short version: ./build.sh -e dev -m install -b 1 -u http://hc.fun'
+  echo ''
+  echo -e "Available arguments are:"
+  echo -e "${bold}\t--env, -e: Environment to build.${normal}"
+  echo -e '\t\tAllowed values are: dev, recette, preprod, prod'
+  echo -e '\t\tDefault value: prod'
+  echo ''
+  echo -e "${bold}\t--mode, -m: Build mode${normal}"
+  echo -e '\t\tAllowed values are: install, update'
+  echo -e '\t\tDefault value: update'
+  echo ''
+  echo -e "${bold}\t--backup, -e: Generates a backup before building the project.${normal}"
+  echo -e '\t\tAllowed values are: 0: does not generate a backup, 1: generates a backup.'
+  echo -e '\t\tDefault value: 1'
+  echo ''
+  echo -e "${bold}\t--uri, -u: Local URL of your project${normal}"
+  echo -e '\t\tUsed when the final drush uli command is runned.'
+  exit
+}
+
 # Working directory.
 # Helper to let you run the install script from anywhere.
 currentscriptpath () {
@@ -134,6 +161,10 @@ do
         ;;
       -u|--uri)
         URI="$2"
+        shift
+        ;;
+      -h|--help)
+        usage
         shift
         ;;
       --) # End of all options
