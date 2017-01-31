2aee1a0b04fcc031df3847f384247971a6f6ce70
Edouard Cunibil <edouard@happyculture.coop>
2017-01-31 18:31:49 +0100

    Issue #1575: get example.post_deploy.sh improvements from rocket_happy.
    Change-Id: Ib3c66f53597ac9ffd7cf9cf49ee7d58c3e583234

--- scripts/example.postdeploy_actions.sh
+++ scripts/example.postdeploy_actions.sh
@@ -12,44 +12,63 @@
 # $FETCH_DB_DUMP: Flag to retrieve a DB dump from the production server.
 # $OFFLINE: Flag if we must avoid remote connections.
 
+BUNDLE=/usr/local/rvm/wrappers/default/bundle
+if [ ! -f /usr/local/rvm/wrappers/default/bundle ]; then
+  BUNDLE=`which bundle`
+fi
+
 # Return error codes if they happen.
 set -e
 
 case $ENV in
   dev)
-    # Examples:
     # Compile CSS for development.
     # (
-    #   cd $WEBROOT/sites/all/themes/custom_theme/;
-    #   compass compile --force -e production
+    #   cd $WEBROOT/sites/all/themes/custom_theme/
+    #   if [ $OFFLINE == 0] ; then $BUNDLE install; fi # reads .bundle/config to find the vendor path.
+    #   $BUNDLE exec compass compile --force -e development
     # )
-
+    #
     # Turn off the aggregation to avoid to turn crazy.
     # $DRUSH vset preprocess_css 0;
     # $DRUSH vset preprocess_js 0;
+    #
     # Enable UIs.
-    # $DRUSH en -y devel field_ui diff views_ui;
+    # $DRUSH en -y devel diff features_ui field_ui views_ui;
+    #
     # Fetch missing images from the remote server.
-    # $DRUSH en -y stage_file_proxy
-    # $DRUSH vset stage_file_proxy_origin "https://www.example.org"
+    # if [ $OFFLINE == 0] ; then
+    #   $DRUSH en -y stage_file_proxy
+    #   $DRUSH vset stage_file_proxy_origin "https://www.example.org"
+    # fi
+    #
     # Connect.
     # $DRUSH uli
     ;;
   recette|preprod)
-    # Examples:
     # Compile CSS for development.
     # (
-    #   cd $WEBROOT/sites/all/themes/custom_theme/;
-    #   compass compile --force -e production
+    #   cd $WEBROOT/sites/all/themes/custom_theme/
+    #   if [ $OFFLINE == 0] ; then $BUNDLE install; fi # reads .bundle/config to find the vendor path.
+    #   $BUNDLE exec compass compile --force -e development
+    # )
+
+    # Fetch missing images from the remote server.
+    # if [ $OFFLINE == 0] ; then
+    #   $DRUSH en -y stage_file_proxy
+    #   $DRUSH vset stage_file_proxy_origin "https://www.example.org"
+    # fi
+    ;;
+  prod)
+    # Compile CSS for production.
+    # (
+    #   cd $WEBROOT/sites/all/themes/custom_theme/
+    #   if [ $OFFLINE == 0] ; then $BUNDLE install; fi # reads .bundle/config to find the vendor path.
+    #   $BUNDLE exec compass compile --force -e production
     # )
 
     # Disable dev modules.
     # $DRUSH dis -y devel field_ui diff views_ui
-    # Fetch missing images from the remote server.
-    # $DRUSH en -y stage_file_proxy
-    # $DRUSH vset stage_file_proxy_origin "https://www.example.org"
-    ;;
-  prod)
     ;;
   *)
     echo "Unknown environment: $ENV. Please check your name."
