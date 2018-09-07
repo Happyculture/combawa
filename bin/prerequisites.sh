d7938aa39a9cd99ddcd81d31d3d362a17569c2d4
Artusamak <julien@happyculture.coop>
2018-09-07 16:37:18 +0200

    Add verification for a config directory.

--- bin/prerequisites.sh
+++ bin/prerequisites.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
 
+# Override default settings per project.
+echo -e "${BLUE}Verifying config directory setup.${NC}"
+echo -e ""
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo -e "${ORANGE}Your config directory does not exist yet.${NC}"
+  while true; do
+    echo "Would you like to create it?"
+    read -p "$CONFIG_DIR [y/N] " yn
+    case $yn in
+        [Yy]* )
+          mkdir $CONFIG_DIR
+          echo -e "${LIGHT_GREEN}Config directory created.${NC}"
+          break;;
+        [Nn]* )
+          echo -e "${LIGHT_CYAN}Config directory not created.${NC}"
+          exit;;
+        "" ) exit;;
+        * ) echo -e "${ORANGE}Please answer yes or no.${NC}";;
+    esac
+  done
+  else
+    echo -e "${GREEN}Config directory... OK!${NC}"
+fi
