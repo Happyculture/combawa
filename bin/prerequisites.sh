1242c898cc9c4c3ccc707eca8ef675049c20156a
Artusamak <julien@happyculture.coop>
2018-09-07 18:05:37 +0200

    Improve app style a readibility. 8-)

--- bin/prerequisites.sh
+++ bin/prerequisites.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+echo -e ""
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo -e ""
+
 # Check config directory existance.
 echo -e ""
 echo -e "${BLUE}Verifying config directory setup.${NC}"
-echo -e ""
 if [ ! -d "$CONFIG_DIR" ]; then
+  echo -e ""
   echo -e "${ORANGE}Your config directory does not exist yet.${NC}"
   while true; do
     echo ''
-    echo "Would you like to create it?"
-    read -p "$CONFIG_DIR [y/N] " yn
+    read -p "Would you like to create it? [y/N] " yn
     case $yn in
         [Yy]* )
           mkdir $CONFIG_DIR
-          echo -e "${LIGHT_GREEN}Config directory created.${NC}"
+          echo -e ""
+          echo -e "${LIGHT_GREEN}Config directory $CONFIG_DIR created.${NC}"
           break;;
         [Nn]* )
           echo -e "${LIGHT_CYAN}Config directory not created.${NC}"
@@ -22,9 +26,9 @@ if [ ! -d "$CONFIG_DIR" ]; then
         * ) echo -e "${ORANGE}Please answer yes or no.${NC}";;
     esac
   done
-  else
-    echo -e "${GREEN}Config directory... OK!${NC}"
 fi
+echo -e ""
+echo -e "${GREEN}Config directory... OK!${NC}"
 
 echo -e ""
 echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
