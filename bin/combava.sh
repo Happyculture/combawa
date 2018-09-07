ed3debcd930707c4a71d0996b6f1aec4917f76ab
Artusamak <julien@happyculture.coop>
2018-09-07 16:09:37 +0200

    Extract functions in there own file.
    Change-Id: I96e8dd9fa92a02bc28335e526ed3027f2fed6c88

--- bin/combava.sh
+++ bin/combava.sh
@@ -20,6 +20,7 @@ currentscriptpath () {
 SCRIPTS_PATH=$(currentscriptpath)
 
 source $SCRIPTS_PATH/colors.sh
+source $SCRIPTS_PATH/functions.sh
 
 # Return error codes if they happen.
 set -e
@@ -45,42 +46,6 @@ FETCH_DB_DUMP=0
 # Override with -o or --offline.
 OFFLINE=0
 
-########## FUNCTION ##############
-# Help function.
-usage() {
-  bold=$(tput bold)
-  normal=$(tput sgr0)
-
-  echo 'Usage:'
-  echo 'Long version: ./build.sh --env dev --mode install --backup 1 --uri http://hc.fun --fetch-db-dump'
-  echo 'Short version: ./build.sh -e dev -m install -b 1 -u http://hc.fun -f'
-  echo ''
-  echo -e "Available arguments are:"
-  echo -e "${bold}\t--env, -e: Environment to build.${normal}"
-  echo -e '\t\tAllowed values are: dev, recette, preprod, prod'
-  echo -e '\t\tDefault value: prod'
-  echo ''
-  echo -e "${bold}\t--mode, -m: Build mode${normal}"
-  echo -e '\t\tAllowed values are: install, update'
-  echo -e '\t\tDefault value: update'
-  echo ''
-  echo -e "${bold}\t--backup, -e: Generates a backup before building the project.${normal}"
-  echo -e '\t\tAllowed values are: 0: does not generate a backup, 1: generates a backup.'
-  echo -e '\t\tDefault value: 1'
-  echo ''
-  echo -e "${bold}\t--uri, -u: Local URL of your project${normal}"
-  echo -e '\t\tUsed when the final drush uli command is runned.'
-  echo ''
-  echo -e "${bold}\t--fetch-db-dump, -f: Fetch a fresh DB dump from the production site.${normal}"
-  echo -e '\t\tUsed when the reference dump should be updated.'
-  echo ''
-  echo -e "${bold}\t--offline, -o: Run offline to avoid trying to make remote connections.${normal}"
-  echo -e '\t\tAllowed values are: 0: make remote connections, 1: avoid remote connections.'
-  echo -e '\t\tDefault value: 0'
-  exit
-}
-
-# Working directory.
 WEBROOT="$SCRIPTS_PATH/../web"
 APP_ROOT="$WEBROOT/.."
 
