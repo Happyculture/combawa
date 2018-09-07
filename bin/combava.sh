4dc805dd2441136079e6009b1b766730b6569533
Artusamak <julien@happyculture.coop>
2018-09-07 16:07:05 +0200

    Start file reordering.
    Change-Id: Ia00af9c6367d3e9f28e4a66d2a980e8c4630da2f

--- bin/combava.sh
+++ bin/combava.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Working directory.
+# Helper to let you run the install script from anywhere.
+currentscriptpath () {
+  SOURCE="${BASH_SOURCE[0]}"
+  # resolve $SOURCE until the file is no longer a symlink
+  while [ -h "$SOURCE" ]; do
+
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo $DIR
+}
+
+# Working directory.
+SCRIPTS_PATH=$(currentscriptpath)
+
 # Return error codes if they happen.
 set -e
 
@@ -60,23 +79,6 @@ usage() {
 }
 
 # Working directory.
-# Helper to let you run the install script from anywhere.
-currentscriptpath () {
-  SOURCE="${BASH_SOURCE[0]}"
-  # resolve $SOURCE until the file is no longer a symlink
-  while [ -h "$SOURCE" ]; do
-
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-  done
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  echo $DIR
-}
-
-# Working directory.
-SCRIPTS_PATH=$(currentscriptpath)
 WEBROOT="$SCRIPTS_PATH/../web"
 APP_ROOT="$WEBROOT/.."
 
