8299d7d336a651161553627ba95d1c46e669c385
Artusamak <julien@happyculture.coop>
2018-09-08 13:12:13 +0200

    Add a preflight script to setup the config as a starting point.

---
+++ bin/pre_flight.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Return error codes if they happen.
+set -e
+
+# Working directory.
+# Helper to let you run the install script from anywhere.
+currentscriptpath () {
+  SOURCE="${BASH_SOURCE[0]}"
+  # resolve $SOURCE until the file is no longer a symlink
+  while [ -h "$SOURCE" ]; do
+
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo $DIR
+}
+
+# Binary variables.
+SCRIPTS_PATH=$(currentscriptpath)
+UTILS_DIR="$SCRIPTS_PATH/../utils"
+TEMPLATES_DIR="$SCRIPTS_PATH/../templates"
+
+# App variables.
+APP_ROOT="$SCRIPTS_PATH/../../../.."
+WEBROOT="$APP_ROOT/web"
+CONFIG_DIR="$APP_ROOT/config"
+APP_SCRIPTS_DIR="$APP_ROOT/scripts"
+
+source $UTILS_DIR/colors.sh
+source $UTILS_DIR/functions.sh
+
+echo -e ""
+echo -e "${LIGHT_PURPLE}"
+echo -e "                 .                 "
+echo -e "             /\ /l                 "
+echo -e "            ((.Y(!                 "
+echo -e "             \ |/          COMBAWA, PLEASED TO SERVE!"
+echo -e "             /  6~6,       Checking prerequisites."
+echo -e "             \ _    +-.            "
+echo -e "              \`-=--^-'            "
+echo -e "               \ \                 "
+echo -e "              _/  \\"
+echo -e "             (  .  Y"
+echo -e "            /\"\ \`--^--v--."
+echo -e "           / _ \`--\"T~\/~\/"
+echo -e "          / \" ~\.  !"
+echo -e "    _    Y      Y./'"
+echo -e "   Y^|   |      |~~7"
+echo -e "   | l   |     / ./'"
+echo -e "   | \`L  | Y .^/~T"
+echo -e "   |  l  ! | |/| |          -Row"
+echo -e "   | .\`\/' | Y | !"
+echo -e "   l  \"~   j l j_L______"
+echo -e "    \,____{ __\"~ __ ,\_,\_"
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+
+source $UTILS_DIR/prerequisites.sh
