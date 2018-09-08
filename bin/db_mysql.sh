e335097fbf5cb38be476858646498edcc4d238bc
Artusamak <julien@happyculture.coop>
2018-09-08 13:12:31 +0200

    Be consistent in Combawa rendering.

--- bin/db_mysql.sh
+++ bin/db_mysql.sh
@@ -1,14 +1,72 @@
 #!/usr/bin/env bash
 
+# Return error codes if they happen.
+set -e
+
+# Working directory.
+# Helper to let you run the install script from anywhere.
+currentscriptpath () {
+  SOURCE="${BASH_SOURCE[0]}"
+  # resolve $SOURCE until the file is no longer a symlink
+  while [ -h "$SOURCE" ]; do
+
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo $DIR
+}
+
+# Binary variables.
+SCRIPTS_PATH=$(currentscriptpath)
+UTILS_DIR="$SCRIPTS_PATH/../utils"
+TEMPLATES_DIR="$SCRIPTS_PATH/../templates"
+
+# App variables.
+APP_ROOT="$SCRIPTS_PATH/../../../.."
+WEBROOT="$APP_ROOT/web"
+CONFIG_DIR="$APP_ROOT/config"
+APP_SCRIPTS_DIR="$APP_ROOT/scripts"
+
+source $UTILS_DIR/colors.sh
+source $UTILS_DIR/functions.sh
+
+echo -e ""
+echo -e "${LIGHT_PURPLE}"
+echo -e "                 .                 "
+echo -e "             /\ /l                 "
+echo -e "            ((.Y(!                 "
+echo -e "             \ |/          COMBAWA, PLEASED TO SERVE!"
+echo -e "             /  6~6,       Checking prerequisites."
+echo -e "             \ _    +-.            "
+echo -e "              \`-=--^-'            "
+echo -e "               \ \                 "
+echo -e "              _/  \\"
+echo -e "             (  .  Y"
+echo -e "            /\"\ \`--^--v--."
+echo -e "           / _ \`--\"T~\/~\/"
+echo -e "          / \" ~\.  !"
+echo -e "    _    Y      Y./'"
+echo -e "   Y^|   |      |~~7"
+echo -e "   | l   |     / ./'"
+echo -e "   | \`L  | Y .^/~T"
+echo -e "   |  l  ! | |/| |          -Row"
+echo -e "   | .\`\/' | Y | !"
+echo -e "   l  \"~   j l j_L______"
+echo -e "    \,____{ __\"~ __ ,\_,\_"
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+
+# @TODO: Load settings from conf file.
+
 # Make drush a variable to use the one shipped with the repository.
 DRUSH="$APP_ROOT/vendor/bin/drush -y --root=$WEBROOT"
 if [ $WEBSITE_URI ]; then
   DRUSH="$DRUSH --uri=$WEBSITE_URI"
 fi
 
-echo "$DRUSH sql:connect"
-echo "AAAA"
-exit
+echo $DRUSH
 
 # Test DB connection.
 echo -e ""
