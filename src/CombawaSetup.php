facea2208bce5e0220902a4eca6d43e912e2d66d
Artusamak <julien@happyculture.coop>
2018-10-02 16:50:09 +0200

    Fix the incorrect dependencies.

--- src/CombawaSetup.php
+++ src/CombawaSetup.php
@@ -2,16 +2,14 @@
 
 namespace happyculture;
 
-use Symfony\Component\Console\Application;
 use Composer\Script\Event;
 use Composer\Util\ProcessExecutor;
 
-class CombawaSetup extends Command {
+class CombawaSetup {
   // @TODO: Document the next steps for the user. Run the db setup script.
   public static function setupScript(Event $event) {
     $io = $event->getIO();
-    $process = new ProcessExecutor($io);
-    $pw = $io->askConfirmation('Enter your Password: ', 'yes');
+    $io->warning('Lancer la commande vendor/bin/combawa.sh pour initialiser le projet.');
   }
 
 }
