466636b4c6ea6a856ee1e1ee9181ceb21893e1c2
Artusamak <julien@happyculture.coop>
2018-10-15 17:07:19 +0200

    Merge Kumquat code generation in Combawa.

---
+++ src/Command/GenerateProjectCommand.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Drupal\Console\Combawa\Command;
+
+use Drupal\Console\Command\Shared\ConfirmationTrait;
+use Drupal\Console\Core\Command\Command;
+use Drupal\Console\Core\Style\DrupalStyle;
+use Drupal\Console\Core\Utils\StringConverter;
+use Drupal\Console\Combawa\Generator\ProjectGenerator;
+use Drupal\Console\Extension\Manager;
+use Drupal\Console\Utils\Validator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateProjectCommand extends Command {
+
+  use ConfirmationTrait;
+
+  /**
+   * @var Manager
+   */
+  protected $extensionManager;
+
+  /**
+   * @var ProjectGenerator
+   */
+  protected $generator;
+
+  /**
+   * @var StringConverter
+   */
+  protected $stringConverter;
+
+  /**
+   * @var Validator
+   */
+  protected $validator;
+
+  /**
+   * ProfileCommand constructor.
+   *
+   * @param Manager          $extensionManager
+   * @param ProjectGenerator $generator
+   * @param StringConverter  $stringConverter
+   * @param Validator        $validator
+   * @param $appRoot
+   */
+  public function __construct(
+    Manager $extensionManager,
+    ProjectGenerator $generator,
+    StringConverter $stringConverter,
+    Validator $validator,
+    $appRoot
+  ) {
+    $this->extensionManager = $extensionManager;
+    $this->generator = $generator;
+    $this->stringConverter = $stringConverter;
+    $this->validator = $validator;
+    $this->appRoot = $appRoot;
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure()
+  {
+    $this
+      ->setName('kumquat:generate-project')
+      ->setAliases(['kgp'])
+      ->setDescription('Generate an install profile, a default theme and an admin theme.')
+      ->addOption(
+        'name',
+        null,
+        InputOption::VALUE_REQUIRED,
+        'The project readable name (ex: Happyculture).'
+      )
+      ->addOption(
+        'machine-name',
+        null,
+        InputOption::VALUE_OPTIONAL,
+        'The project (short) machine name (ex: hc).'
+      );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    // @see use Drupal\Console\Command\Shared\ConfirmationTrait::confirmOperation
+    if (!$this->confirmOperation()) {
+      return 1;
+    }
+
+    $name = $this->validator->validateModuleName($input->getOption('name'));
+    $machine_name = $this->validator->validateMachineName($input->getOption('machine-name'));
+
+    $this->generator->generate([
+      'name' => $name,
+      'machine_name' => $machine_name,
+      'profiles-dir' => 'profiles',
+      'themes-dir' => 'themes/custom'
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+    $validators = $this->validator;
+
+    try {
+      // A profile is technically also a module, so we can use the same
+      // validator to check the name.
+      $name = $input->getOption('name') ? $validators->validateModuleName($input->getOption('name')) : null;
+    } catch (\Exception $error) {
+      $this->getIo()->error($error->getMessage());
+
+      return 1;
+    }
+
+    if (!$name) {
+      $name = $this->getIo()->ask(
+        'What is the human readable name of the project?',
+        '',
+        function ($name) use ($validators) {
+          return $validators->validateModuleName($name);
+        }
+      );
+      $input->setOption('name', $name);
+    }
+
+    try {
+      $machine_name = $input->getOption('machine-name') ? $validators->validateMachineName($input->getOption('machine-name')) : null;
+    } catch (\Exception $error) {
+      $this->getIo()->error($error->getMessage());
+
+      return 1;
+    }
+
+    if (!$machine_name) {
+      $machine_name = $this->getIo()->ask(
+        'What is the machine name of the project?',
+        $this->stringConverter->createMachineName($name),
+        function ($machine_name) use ($validators) {
+          return $validators->validateMachineName($machine_name);
+        }
+      );
+      $input->setOption('machine-name', $machine_name);
+    }
+  }
+
+}
---
+++ src/Generator/ProjectGenerator.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\Console\Combawa\Generator;
+
+use Drupal\Console\Core\Generator\Generator;
+
+class ProjectGenerator extends Generator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function generate(array $parameters) {
+    $profiles_dir = $parameters['profiles-dir'];
+    $themes_dir = $parameters['themes-dir'];
+    $machine_name = $parameters['machine_name'];
+
+    // Check directories.
+    $this->checkDir(($profiles_dir == '/' ? '' : $profiles_dir) . '/' . $machine_name);
+    $this->checkDir(($themes_dir == '/' ? '' : $themes_dir) . '/' . $machine_name . '_theme');
+    $this->checkDir(($themes_dir == '/' ? '' : $themes_dir) . '/' . $machine_name . '_admin_theme');
+
+    // Generate profile.
+    $profilePath = ($profiles_dir == '/' ? '' : $profiles_dir) . '/' . $machine_name . '_theme' . '/' . $machine_name . '_theme';
+    $profileParameters = [
+      'profile' => $parameters['name'],
+      'machine_name' => $machine_name,
+      'themes' => [ $machine_name . '_theme', $machine_name . '_admin_theme' ],
+    ];
+
+    $this->renderFile(
+      'combawa-profile/info.yml.twig',
+      $profilePath . '.info.yml',
+      $profileParameters
+    );
+
+    $this->renderFile(
+      'combawa-profile/profile.twig',
+      $profilePath . '.profile',
+      $profileParameters
+    );
+
+    $this->renderFile(
+      'combawa-profile/install.twig',
+      $profilePath . '.install',
+      $profileParameters
+    );
+
+    // Generate default theme.
+    $defaultThemePath = ($profiles_dir == '/' ? '' : $profiles_dir) . '/' . $machine_name . '_theme' . '/' . $machine_name . '_theme';
+    $defaultThemeParameters = [
+      'profile' => $parameters['name'],
+      'theme' => $parameters['name'],
+      'machine_name' => $machine_name . '_theme',
+    ];
+    // TODO continue
+
+    // Generate admin theme.
+    $adminThemePath = ($profiles_dir == '/' ? '' : $profiles_dir) . '/' . $machine_name . '_admin_theme' . '/' . $machine_name . '_admin_theme';
+    $adminThemeParameters = [
+      'profile' => $parameters['name'],
+      'theme' => $parameters['name'] . ' Admin',
+      'machine_name' => $machine_name . '_admin_theme',
+    ];
+
+    $this->renderFile(
+      'combawa-admin-theme/info.yml.twig',
+      $adminThemePath . '.info.yml',
+      $adminThemeParameters
+    );
+
+    $this->renderFile(
+      'combawa-admin-theme/theme.twig',
+      $adminThemePath . '.theme',
+      $adminThemeParameters
+    );
+
+    $this->renderFile(
+      'combawa-admin-theme/libraries.yml.twig',
+      $adminThemePath . '.libraries.yml',
+      $adminThemeParameters
+    );
+  }
+
+  /**
+   * @param $dir
+   * @throws \RuntimeException
+   */
+  protected function checkDir($dir) {
+    if (file_exists($dir)) {
+      if (!is_dir($dir)) {
+        throw new \RuntimeException(
+          sprintf(
+            'Unable to generate the profile as the target directory "%s" exists but is a file.',
+            realpath($dir)
+          )
+        );
+      }
+      $files = scandir($dir);
+      if ($files != ['.', '..']) {
+        throw new \RuntimeException(
+          sprintf(
+            'Unable to generate the profile as the target directory "%s" is not empty.',
+            realpath($dir)
+          )
+        );
+      }
+      if (!is_writable($dir)) {
+        throw new \RuntimeException(
+          sprintf(
+            'Unable to generate the profile as the target directory "%s" is not writable.',
+            realpath($dir)
+          )
+        );
+      }
+    }
+  }
+
+}
