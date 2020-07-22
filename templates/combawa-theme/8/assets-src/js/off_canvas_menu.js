118267ec0bcaa52ecdc2e1611577e09c3c07649a
Edouard Cunibil <edouard@happyculture.coop>
2020-07-22 10:32:53 +0200

    Convert off-canvas to vanilla JS.
    Change-Id: Iad5def971a36976ada52f01cb9ff4a40142e835a

--- templates/combawa-theme/8/assets-src/js/off_canvas_menu.js
+++ templates/combawa-theme/8/assets-src/js/off_canvas_menu.js
@@ -1,49 +1,127 @@
-(function (Drupal, debounce, $) {
+(function (Drupal, debounce, document, window) {
 
-  function handleResize() {
-    $('.off-canvas-menu__button:not(.off-canvas-menu__button--close)').each(function () {
-      var $target = $('#' + $(this).attr('aria-controls'));
-      // If the control button is hidden, show the target.
-      if (!$(this).is(':visible')) {
-        $target.removeAttr('aria-hidden');
-        $(this).parent().find('.off-canvas-menu__button')
-          .attr('aria-expanded', 'true');
-      }
-      // If the button is visible and the canvas has not been opened by a
-      // previous click, hide it the canvas.
-      else if (!$(this).data('off-canvas-open-by-click')) {
-        $target.attr('aria-hidden', 'true');
-        $(this).parent().find('.off-canvas-menu__button')
-          .attr('aria-expanded', 'false');
-      }
+  /**
+   * Filters out the list so any item has never been called with this id.
+   *
+   * @param NodeList|array list
+   * @param string id
+   * @returns array
+   */
+  const once = (list, id) => {
+    const key = 'data-once-' + id;
+    const filtered = Array.prototype.filter.call(list, (item) => {
+      return !item.hasAttribute(key);
     });
-  }
+    filtered.forEach((item) => {
+      item.setAttribute(key, 'true');
+    });
+    return filtered;
+  };
 
-  Drupal.behaviors.OffCanvasMenu = {
-    attach: function () {
-      $('.off-canvas-menu__button').once('OffCanvasMenu').on('click', function () {
-        var $target = $('#' + $(this).attr('aria-controls'));
-        if ($(this).attr('aria-expanded') === 'true') {
-          $target.attr('aria-hidden', 'true');
-          $(this).parent().find('.off-canvas-menu__button')
-            .attr('aria-expanded', 'false')
-            .removeData('off-canvas-open-by-click');
-          $(this).parents("header").removeClass('with-menu');
-          $("body").removeClass("menu-displayed");
+  /**
+   * Gets all element ancestors that matches the selector.
+   *
+   * @param HTMLElement el
+   * @param string selector
+   * @returns array
+   */
+  const parents = (el, selector) => {
+    let current = el,
+      list = [];
+    while(current.parentNode != null && current.parentNode !== document.documentElement) {
+      if (selector.length === 0 || current.parentNode.matches(selector)) {
+        list.push(current.parentNode);
+      }
+      current = current.parentNode;
+    }
+    return list
+  };
+
+  /**
+   * Function called when the window is resized.
+   *
+   * Show or hide the off screen canvas according to the button state when it
+   * becomes hidden in CSS.
+   */
+  const onWindowResize = () => {
+    document.querySelectorAll('.off-canvas-menu__button:not(.off-canvas-menu__button--close)')
+      .forEach((buttonEl) => {
+        const target = document.getElementById(buttonEl.getAttribute('aria-controls'));
+
+        // If the control button is hidden, show the target.
+        if (buttonEl.offsetParent === null) {
+          target.removeAttribute('aria-hidden');
+          buttonEl.parentNode.querySelectorAll('.off-canvas-menu__button')
+            .forEach((el) => {
+              el.setAttribute('aria-expanded', 'true');
+            });
         }
-        else {
-          $target.removeAttr('aria-hidden');
-          $(this).parent().find('.off-canvas-menu__button')
-            .attr('aria-expanded', 'true')
-            .data('off-canvas-open-by-click', true);
-          $(this).parents("header").addClass("with-menu");
-          $("body").addClass("menu-displayed");
+          // If the button is visible and the canvas has not been opened by a
+        // previous click, hide the target.
+        else if (!buttonEl.getAttribute('data-off-canvas-open-by-click')) {
+          target.setAttribute('aria-hidden', 'true');
+          buttonEl.parentNode.querySelectorAll('.off-canvas-menu__button')
+            .forEach((el) => {
+              el.setAttribute('aria-expanded', 'false');
+            });
         }
       });
+  };
 
-      $(window).once('OffCanvasMenu').on('resize', debounce(handleResize, 50));
-      handleResize();
+  /**
+   * Function called when an off canvas trigger button is clicked.
+   *
+   * Show or hide the off screen canvas according to the button state.
+   *
+   * @param event
+   */
+  const onButtonClick = (event) => {
+    const currentTargetEl = event.currentTarget;
+    const target = document.getElementById(currentTargetEl.getAttribute('aria-controls'));
+
+    if (currentTargetEl.getAttribute('aria-expanded') === 'true') {
+      target.setAttribute('aria-hidden', 'true');
+      parents(target, 'header')
+        .forEach((parentEl) => {
+          parentEl.querySelectorAll('.off-canvas-menu__button')
+            .forEach((buttonEl) => {
+              buttonEl.setAttribute('aria-expanded', 'false');
+              buttonEl.removeAttribute('data-off-canvas-open-by-click');
+            });
+          parentEl.classList.remove('with-menu');
+        });
+      document.querySelector('body')
+        .classList.remove('menu-displayed');
+    }
+    else {
+      target.removeAttribute('aria-hidden');
+      parents(target, 'header')
+        .forEach((parentEl) => {
+          parentEl.querySelectorAll('.off-canvas-menu__button')
+            .forEach((buttonEl) => {
+              buttonEl.setAttribute('aria-expanded', 'true');
+              buttonEl.setAttribute('data-off-canvas-open-by-click', 'true');
+            });
+          parentEl.classList.add('with-menu');
+        });
+      document.querySelector('body')
+        .classList.add('menu-displayed');
     }
   };
 
-})(Drupal, Drupal.debounce, jQuery);
+  Drupal.behaviors.OffCanvasMenu = {
+    attach: function () {
+      const buttons = document.querySelectorAll('.off-canvas-menu__button');
+      once(buttons, 'OffCanvasMenu').forEach((el) => {
+        el.addEventListener('click', onButtonClick);
+      });
+
+      const body = document.querySelectorAll('body');
+      once(body, 'OffCanvasMenu').forEach((el) => {
+        window.addEventListener('resize', debounce(onWindowResize, 50));
+        onWindowResize();
+      });
+    }
+  };
+
+})(Drupal, Drupal.debounce, document, window);
