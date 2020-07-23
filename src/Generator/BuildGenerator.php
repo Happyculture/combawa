8581b3fd520e65980a8c764b1b638bfaf883fd89
Edouard Cunibil <edouard@happyculture.coop>
2020-07-23 15:04:44 +0200

    Remove project build generation from combawa (will be recreated as a standalone package).
    Change-Id: Idb8a769804bb269e0dcd9af581f5449c27e6511b

--- src/Generator/BuildGenerator.php
+++ src/Generator/BuildGenerator.php
@@ -2,22 +2,43 @@
 
 namespace Drupal\Console\Combawa\Generator;
 
+use Drupal\Console\Core\Generator\Generator;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Yaml;
 
-class BuildGenerator extends ProjectGenerator {
+class BuildGenerator extends Generator {
+
+  const TPL_DIR = __DIR__ . '/../../templates';
 
   /**
-   * Generates all the stuff.
+   * Generates the build scripts.
    *
    * Method called by the combawa:generate-build command.
    *
    * @param array $parameters
    */
   public function generate(array $parameters) {
-    $parameters['generate_build'] = TRUE;
-    $this->core_version = $parameters['core'];
-    $this->generateBuild($parameters);
+    $scripts_folder = '../scripts';
+
+    $buildParameters = [
+      'name' => $parameters['name'],
+      'machine_name' => $parameters['machine_name'],
+      'production_url' => $parameters['url'],
+    ];
+
+    $dir = opendir(self::TPL_DIR . '/combawa-build/' . $parameters['core'] . '');
+    while ($file = readdir($dir)) {
+      if ($file[0] === '.') {
+        continue;
+      }
+
+      $destination_file = substr($file, 0, -1 * strlen('.twig'));
+      $this->renderFile(
+        'combawa-build/' . $parameters['core'] . '/' . $file,
+        $scripts_folder . '/' . $destination_file,
+        $buildParameters
+      );
+    }
   }
 
 }
