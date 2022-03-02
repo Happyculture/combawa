1c9a5655de5d3bd3d06fb96db4c9fe6cde73a40e
Artusamak <julien@happyculture.coop>
2022-03-02 10:11:30 +0100

    Do not force the build mode when generating the scripts.

--- src/Generator/InitializeBuildScriptsGenerator.php
+++ src/Generator/InitializeBuildScriptsGenerator.php
@@ -20,6 +20,7 @@ class InitializeBuildScriptsGenerator extends Generator {
 
     $buildParameters = [
       'machine_name' => $parameters['machine_name'],
+      'build_mode' => $parameters['build_mode'],
     ];
 
     // Alter composer.json file.
@@ -27,7 +28,7 @@ class InitializeBuildScriptsGenerator extends Generator {
     chdir($this->drupalFinder->getComposerRoot());
     exec('/usr/bin/env composer config name ' . $parameters['machine_name'] . '/' . $parameters['machine_name']);
     exec('/usr/bin/env composer config extra.combawa.machine_name ' . $parameters['machine_name']);
-    exec('/usr/bin/env composer config extra.combawa.build_mode install');
+    exec('/usr/bin/env composer config extra.combawa.build_mode ' . $parameters['build_mode']);
     exec('/usr/bin/env composer update --lock');
     chdir($prevDir);
     $this->fileQueue->addFile('../composer.json');
