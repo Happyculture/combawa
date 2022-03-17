27969b13ade185704c3a04a0c2a4f9da11173b24
Edouard Cunibil <edouard@happyculture.coop>
2022-03-17 11:31:21 +0100

    Issue #90: remove machine name collection.
    Change-Id: I3c34c22332c8d513ad3b858a28c1b5974896bd0d

--- src/Command/InitializeBuildScriptsCommand.php
+++ src/Command/InitializeBuildScriptsCommand.php
@@ -10,13 +10,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
-use Drupal\Console\Combawa\Command\GenerateEnvironmentCommand;
 
 class InitializeBuildScriptsCommand extends Command {
 
   use ConfirmationTrait;
-
-  const REGEX_MACHINE_NAME = '/^[a-z0-9_]+$/';
 
   /**
    * @var InitializeBuildScriptsGenerator
@@ -62,12 +59,6 @@ class InitializeBuildScriptsCommand extends Command {
         null,
         InputOption::VALUE_REQUIRED,
         'The build module to use (install or update) if wanted.',
-      )
-      ->addOption(
-        'machine-name',
-        null,
-        InputOption::VALUE_OPTIONAL,
-        'The project (short) machine name (ex: hc).'
       );
   }
 
@@ -80,11 +71,6 @@ class InitializeBuildScriptsCommand extends Command {
       ['Build mode', $build_mode],
     ];
 
-    if (!empty($input->getOption('machine-name'))) {
-      $machine_name = $this->validateMachineName(GenerateEnvironmentCommand::validateOptionalValueWhenRequested($input->getOption('machine-name'), 'machine-name'));
-      $recap_params[] = ['Machine name', $machine_name];
-    }
-
     $this->getIo()->newLine(1);
     $this->getIo()->commentBlock('Settings recap');
     $this->getIo()->table(['Parameter', 'Value'], $recap_params);
@@ -95,7 +81,6 @@ class InitializeBuildScriptsCommand extends Command {
     }
 
     $this->generator->generate([
-      'machine_name' => $machine_name,
       'build_mode' => $build_mode,
     ]);
     if ($this->run_gen_env_command) {
@@ -128,53 +113,9 @@ class InitializeBuildScriptsCommand extends Command {
       $input->setOption('build-mode', $build_mode);
     }
 
-    if ($build_mode == 'install') {
-      try {
-        $machine_name = $input->getOption('machine-name') ? $this->validateMachineName($input->getOption('machine-name')) : null;
-      } catch (\Exception $error) {
-        $this->getIo()->error($error->getMessage());
-
-        return 1;
-      }
-
-      if (!$machine_name) {
-        $machine_name = $this->getIo()->ask(
-          'What is the machine name of your install profile?',
-          'new_project',
-          function ($machine_name) {
-            return $this->validateMachineName(GenerateEnvironmentCommand::validateOptionalValueWhenRequested($machine_name, 'machine-name'));
-          }
-        );
-        $input->setOption('machine-name', $machine_name);
-      }
-    }
-
     $this->run_gen_env_command = $this->getIo()->confirm(
       'Next, we will need you to generate the environment file (.env). Do you want to do it right after saving the previous settings?',
       TRUE);
-  }
-
-  /**
-   * Validates a machine name.
-   *
-   * @param string $machine_name
-   *   The machine name.
-   * @return string
-   *   The machine name.
-   * @throws \InvalidArgumentException
-   */
-  protected function validateMachineName($machine_name) {
-    if (preg_match(static::REGEX_MACHINE_NAME, $machine_name)) {
-      return $machine_name;
-    }
-    else {
-      throw new \InvalidArgumentException(
-        sprintf(
-          'Machine name "%s" is invalid, it must contain only lowercase letters, numbers and underscores.',
-          $machine_name
-        )
-      );
-    }
   }
 
   /**
