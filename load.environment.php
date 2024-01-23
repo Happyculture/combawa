26727f80656c80d360430bdd82ebb8eed8029059
Edouard Cunibil <edouard@happyculture.coop>
2024-01-23 15:50:03 +0100

    Set \Symfony\Component\Dotenv\Dotenv::usePutenv(TRUE) for BC.
    Change-Id: I0f91613f7116ff0ae72293aee612cfdfd0e4ded7

--- load.environment.php
+++ load.environment.php
@@ -4,6 +4,8 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Dotenv\Exception\PathException;
 
 $dotenv = new Dotenv();
+// Backward compatibility with scripts using getenv() like drush.
+$dotenv->usePutenv(TRUE);
 try {
   // This file is located in our dependency (vendor/happyculture/combawa) so we have to climb up the arborescence
   // to load the environment files expected to be at the root level or our project repository.
